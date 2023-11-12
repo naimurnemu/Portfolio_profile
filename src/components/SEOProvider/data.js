b7b5cdd8d676ae60b6fdef253071d88fc84d09e8
naimurnemu <naimur.rahman@mimothi.com>
2023-11-12 13:48:52 +0600

    title and file changed

--- src/components/SEOProvider/data.js
+++ src/components/SEOProvider/data.js
@@ -1,5 +1,5 @@
 export const metaData = {
-  title: "Naimur Rahman - Front-End Developer",
+  title: "Naimur Rahman - Web Developer",
   description: "Passionate front-end developer skilled in JavaScript, TypeScript ReactJS, Material, GraphQL, Redux, and NextJs. Let's create amazing digital experiences together!",
   linkedInUrl: "https://www.linkedin.com/in/naimurnemu/",
   linkedInIma: "https://media.licdn.com/dms/image/D5635AQE1mCQReBY-NA/profile-framedphoto-shrink_200_200/0/1699500716308?e=1700154000&v=beta&t=pTKyOnUqxwk6GCgxc6em8r480MVaJiFk1Pe92XZjYZM",
