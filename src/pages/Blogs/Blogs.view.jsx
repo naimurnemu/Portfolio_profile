8743a43971f1fccb766e2c797726be7c3ac8e30c
Naimur Rahman <naimurrahaman82@gmail.com>
2026-08-22 23:05:11 +0600

    implemented the new section blogs

--- src/pages/Blogs/Blogs.view.jsx
+++ src/pages/Blogs/Blogs.view.jsx
@@ -1,32 +1,63 @@
 import React from "react";
-import { Button } from "react-bootstrap";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
 import { HelmetProvider } from "react-helmet-async";
-import { Link } from "react-router-dom";
 import { SEOProvider } from "../../components";
+import { articles } from "./data";
+import "./Blogs.css";
 
 const Blogs = () => {
   return (
     <HelmetProvider>
-      <SEOProvider title="Blogs" />
-      <div className="d-flex justify-content-center" style={{ width: "100%" }}>
-      <div className="text-center">
-        <img
-          className="img-fluid"
-          src="https://i.ibb.co/Yf8y8BG/Coming-soon-neon-banner-vector-template-Glowing-night-bright-lettering-sign-for-advertisement.jpg"
-          alt="coming-soon"
-        />
-        <br />
-        <br />
-        <br />
-        <Link to="/">
-          <Button className="fs-4 text-primary" variant="">
-            Go Back
-          </Button>
-        </Link>
-      </div>
-    </div>
+      <Container className="blogs-page">
+        <SEOProvider title="Blogs" />
+        <header className="blogs-header animate__animated animate__fadeInUp">
+          <p className="blogs-kicker">Thoughts, notes & experiments</p>
+          <h1>Writing on Medium</h1>
+          <p className="blogs-intro">
+            A collection of practical lessons from building products,
+            learning new tools, and working through the details.
+          </p>
+        </header>
+
+        <Row className="g-4 pb-5">
+          {articles.map((article) => (
+            <Col key={article.id} xs={12} md={6} lg={4}>
+              <Card className="blog-card h-100 animate__animated animate__fadeInUp">
+                <a
+                  className="blog-thumbnail-link"
+                  href={article.link}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <Card.Img
+                    className="blog-thumbnail"
+                    variant="top"
+                    src={article.thumbnail}
+                    alt=""
+                  />
+                </a>
+                <Card.Body className="d-flex flex-column">
+                  <p className="blog-meta">MEDIUM / {article.category}</p>
+                  <Card.Title>{article.title}</Card.Title>
+                  <Card.Text className="blog-caption">
+                    {article.caption}
+                  </Card.Text>
+                  <Button
+                    className="blog-link mt-auto align-self-start"
+                    href={article.link}
+                    target="_blank"
+                    rel="noreferrer"
+                    variant="outline-danger"
+                  >
+                    Read article <span aria-hidden="true">↗</span>
+                  </Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </HelmetProvider>
-    
   );
 };
 
---
+++ src/pages/Blogs/data.js
@@ -0,0 +1,12 @@
+export const articles = [
+  {
+    id: 1,
+    category: "TypeScript",
+    title: "How TypeScript OOP Actually Works",
+    caption:
+      "We all know that object-oriented programming in JavaScript and TypeScript...",
+    link: "https://medium.com/tag/javascript",
+    thumbnail:
+      "https://images.unsplash.com/photo-1627398242454-45a1465c2479?auto=format&fit=crop&w=900&q=80",
+  },
+];
